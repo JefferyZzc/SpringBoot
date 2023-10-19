a7c5145b72b56d649bde31685417509d54294af5
JerreryZzc <1045288412@qq.com>
2023-10-19 22:47:56 +0800

    基本通过JavaMailSender接口和实现类实现了邮件发送功能

---
+++ email-group-sending/src/main/java/emailgroupsending/EmailGroupSendingApplication.java
@@ -0,0 +1,13 @@
+package emailgroupsending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailGroupSendingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailGroupSendingApplication.class, args);
+    }
+
+}
