8eb1cbab3beca91ef6936e6dc91ae2ac7718336e
JerreryZzc <1045288412@qq.com>
2023-10-19 16:17:20 +0800

    测试类中测试了Jwt令牌的创建和解析 创建了jwt工具类 在loginController类中实现登陆成功则发送jwt令牌的功能

--- springboot-case/springboot-case-code/springboot-case/src/main/java/com/jeffery/controller/loginController.java
+++ springboot-case/springboot-case-code/springboot-case/src/main/java/com/jeffery/controller/loginController.java
@@ -3,11 +3,15 @@ package com.jeffery.controller;
 import com.jeffery.pojo.Emp;
 import com.jeffery.pojo.Result;
 import com.jeffery.service.EmpService;
+import com.jeffery.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -19,6 +23,18 @@ public class loginController {
     public Result login(@RequestBody Emp emp){
         log.info("员工登录#{}",emp);
         Emp e = empService.login(emp);
-        return e != null?Result.success():Result.error("用户民或密码错误");
+
+        //登陆成功 生成令牌并返回令牌
+        if(e!=null){
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",emp.getId());
+            claims.put("name",emp.getName());
+            claims.put("username",emp.getUsername());
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+
+        //登陆失败返回错误信息
+        return Result.error("用户民或密码错误");
     }
 }
--- springboot-case/springboot-case-code/springboot-case/src/test/java/com/jeffery/SpringbootCaseApplicationTests.java
+++ springboot-case/springboot-case-code/springboot-case/src/test/java/com/jeffery/SpringbootCaseApplicationTests.java
@@ -1,9 +1,14 @@
 package com.jeffery;
 
+import io.jsonwebtoken.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//@SpringBootTest
 class SpringbootCaseApplicationTests {
 
     @Test
@@ -11,4 +16,28 @@ class SpringbootCaseApplicationTests {
 
     }
 
+    @Test
+    public void testGetJwt(){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id",1);
+        claims.put("name","Tom");
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256,"jeffery")//签名算法
+                .setClaims(claims)//自定义内容
+                .setExpiration(new Date(System.currentTimeMillis()+3600*1000))//设置有效期为一个小时
+                .compact();
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void testParseJwt(){
+        Claims claims = Jwts.parser()
+                .setSigningKey("jeffery")//指定密钥
+                //传递jwt令牌
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiVG9tIiwiaWQiOjEsImV4cCI6MTY5NzcwNTM1Mn0.snA7ur57NeIgOQcgO_zq2xF7rIpapqPeAWPAU2InMeg")
+                //.getHeader();
+                .getBody();//拿到第二部分自定义内容
+                //.getSignature();
+        System.out.println(claims);
+    }
 }
