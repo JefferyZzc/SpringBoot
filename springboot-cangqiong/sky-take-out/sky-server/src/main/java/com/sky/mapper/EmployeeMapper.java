2e955253a566a81baec41fbf5e85b1bd0c1b52a6
JerreryZzc <1045288412@qq.com>
2023-10-25 19:49:15 +0800

    实现员工账号启用禁用功能

--- springboot-cangqiong/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ springboot-cangqiong/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -19,8 +19,10 @@ public interface EmployeeMapper {
     Employee getByUsername(String username);
 
     @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) "+
-    "values (#{name},#{username},#{password},#{phone},#{sex},#{id_number},#{create_time},#{update_time},#{create_user},#{update_user})")
+    "values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
 
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void update(Employee employee);
 }
