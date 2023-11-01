efedb209738f786e7a8daae3985a747172f5f883
JerreryZzc <1045288412@qq.com>
2023-11-01 15:07:53 +0800

    完成微信支付 查看历史和订单记录相关功能

--- springboot-cangqiong/sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ springboot-cangqiong/sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -12,4 +12,7 @@ public interface UserMapper {
     User getByOpenid(String openid);
 
     Integer insert(User user);
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
